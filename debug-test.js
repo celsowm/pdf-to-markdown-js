8fe1ecc31573efdd7ba7db228733047eb5ba268d
celsowm <celsowm@gmail.com>
2026-04-22 14:34:15 -0300

    chore: cleanup temporary debug files and consolidate workspace

--- debug-test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const { PdfReader, PdfParser } = require('./dist/index.js');
-const buffer = fs.readFileSync('tests/fixtures/simple-text.pdf');
-const reader = PdfReader.fromBuffer(buffer);
-const parser = new PdfParser(reader, []);
-const ast = parser.parse();
-console.log('AST:', JSON.stringify(ast, null, 2));
--- patch-csparser-infinite.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let code = fs.readFileSync('src/core/ContentStreamParser.ts', 'utf8');
-
-code = code.replace(/      if \(text \!== null && this\.streamContent\[this\.position\] === 'T'\) \{/, `      this.skipWhitespace();
-      if (text !== null && this.position < this.streamContent.length && this.streamContent[this.position] === 'T' && this.streamContent[this.position + 1] === 'j') {`);
-
-fs.writeFileSync('src/core/ContentStreamParser.ts', code);
